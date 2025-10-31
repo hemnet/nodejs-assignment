4b95caa28c93582b93f2140b1dc522dd84785239
Lucas Santos <lucas.santos@hemnet.se>
2025-10-31 17:58:58 +0100

    feat: add uptime

--- index.ts
+++ index.ts
@@ -3,19 +3,27 @@ import { sequelizeConnection } from './db/config.ts'
 import { seedDb } from './db/seed.ts'
 import packagesRoutes from './routes/package.routes.ts'
 
-const port = 3000
+const port = Number(process.env.PORT ?? 3000)
+const startDate = Date.now()
 export const app = express()
+
+//  Initialize database //
+await sequelizeConnection.sync({ force: true })
+console.log('DB running')
+await seedDb()
+
+app.use(express.json())
+
+app.get('/ping', async (_req, res) => {
+  res.json({
+    status: 'ok',
+    timestamp: Date.now(),
+    uptime: Date.now() - startDate,
+  })
+})
+
+app.use('/api/packages', packagesRoutes)
 
 app.listen(port, () => {
   console.log(`Hemnet application running on port ${port}!`)
 })
-app.use(express.json())
-
-//  Initialize database //
-sequelizeConnection.sync({ force: true }).then(async () => {
-  console.log('DB running')
-
-  await seedDb()
-})
-
-app.use('/api/packages', packagesRoutes)
